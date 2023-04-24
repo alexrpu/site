565c72c0f4c70544dc340287bcd3ac5e41389ee4
Alex Pu <alexander.r.pu@gmail.com>
2023-04-24 01:58:02 -0700

    added basic routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./Home";
+import About from "./About";
+import Portfolio from "./Portfolio";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Basic Example</h1>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="portfolio" element={<Portfolio />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Layout.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { Outlet } from 'react-router-dom'
+
+export class Layout extends Component {
+  render() {
+    return (
+      <div>
+        <nav>
+        <ul>
+            <li>
+            <Link to="/">Home</Link>
+            </li>
+            <li>
+            <Link to="/about">About</Link>
+            </li>
+            <li>
+            <Link to="/portfolio">Portfolio</Link>
+            </li>
+        </ul>
+        </nav>
+
+        <Outlet />
+
+        <footer>
+            <p>Alex Pu</p>
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default Layout
