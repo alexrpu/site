9f1626dbcdd3f09daf7e7ce9edf1279e42dfc0e2
Alex Pu <alexander.r.pu@gmail.com>
2023-04-24 02:14:56 -0700

    switch to hashrouter and fixed paths

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
         </Route>
       </Routes>
     </div>
