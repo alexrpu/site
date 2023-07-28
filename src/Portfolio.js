656a1303b005daa97675766c9720e78d0f5e917c
Alex Pu <alexander.r.pu@gmail.com>
2023-07-27 18:52:34 -0700

    attempt to fix initial flicker

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -15,7 +15,7 @@ function Portfolio() {
     return (
       <div class="px-4">
       <ol class="relative border-l border-background dark:border-accent " >                  
-      <li ref={ref} class={inView ? "mb-10 ml-4 motion-safe:animate-fadeIn" : "mb-10 ml-4"}>
+      <li ref={ref} class={inView ? "mb-10 ml-4 motion-safe:animate-fadeIn opacity-0" : "mb-10 ml-4 opacity-0"}>
           <div class="absolute w-3 h-3 bg-background rounded-full mt-1.5 -left-1.5 border border-text dark:border-background dark:bg-accent"></div>
           <time class="mb-1 text-sm font-normal leading-none text-primary dark:text-accent">February 2022</time>
           <h3 class="text-lg font-semibold text-background dark:text-text">Application UI code in Tailwind CSS</h3>
@@ -24,19 +24,19 @@ function Portfolio() {
       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
     </svg></a>
       </li>
-      <li ref={ref2} class={inView2 ? "mb-10 ml-4 motion-safe:animate-fadeIn" : "mb-10 ml-4"}>
+      <li ref={ref2} class={inView2 ? "mb-10 ml-4 motion-safe:animate-fadeIn opacity-0" : "mb-10 ml-4 opacity-0"}>
           <div class="absolute w-3 h-3 bg-background rounded-full mt-1.5 -left-1.5 border border-text dark:border-background dark:bg-accent"></div>
           <time class="mb-1 text-sm font-normal leading-none text-primary dark:text-accent">March 2022</time>
           <h3 class="text-lg font-semibold text-background dark:text-text">Marketing UI design in Figma</h3>
           <p class="text-base font-normal text-accent dark:text-primary">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
       </li>
-      <li ref={ref3} class={inView3 ? "mb-10 ml-4 motion-safe:animate-fadeIn" : "mb-10 ml-4"}>
+      <li ref={ref3} class={inView3 ? "mb-10 ml-4 motion-safe:animate-fadeIn opacity-0" : "mb-10 ml-4 opacity-0"}>
           <div class="absolute w-3 h-3 bg-background rounded-full mt-1.5 -left-1.5 border border-text dark:border-background dark:bg-accent"></div>
           <time class="mb-1 text-sm font-normal leading-none text-primary dark:text-accent">April 2022</time>
           <h3 class="text-lg font-semibold text-background dark:text-text">E-Commerce UI code in Tailwind CSS</h3>
           <p class="text-base font-normal text-accent dark:text-primary">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
       </li>
-      <li ref={ref4} class={inView4 ? "mb-10 ml-4 motion-safe:animate-fadeIn" : "mb-10 ml-4"}>
+      <li ref={ref4} class={inView4 ? "mb-10 ml-4 motion-safe:animate-fadeIn opacity-0" : "mb-10 ml-4 opacity-0"}>
           <div class="absolute w-3 h-3 bg-background rounded-full mt-1.5 -left-1.5 border border-text dark:border-background dark:bg-accent"></div>
           <time class="mb-1 text-sm font-normal leading-none text-primary dark:text-accent">February 2022</time>
           <h3 class="text-lg font-semibold text-background dark:text-text">Application UI code in Tailwind CSS</h3>
@@ -45,19 +45,19 @@ function Portfolio() {
       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
     </svg></a>
       </li>
-      <li ref={ref5} class={inView5 ? "mb-10 ml-4 motion-safe:animate-fadeIn" : "mb-10 ml-4"}>
+      <li ref={ref5} class={inView5 ? "mb-10 ml-4 motion-safe:animate-fadeIn opacity-0" : "mb-10 ml-4 opacity-0"}>
           <div class="absolute w-3 h-3 bg-background rounded-full mt-1.5 -left-1.5 border border-text dark:border-background dark:bg-accent"></div>
           <time class="mb-1 text-sm font-normal leading-none text-primary dark:text-accent">March 2022</time>
           <h3 class="text-lg font-semibold text-background dark:text-text">Marketing UI design in Figma</h3>
           <p class="text-base font-normal text-accent dark:text-primary">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
       </li>
-      <li ref={ref6} class={inView6 ? "mb-10 ml-4 motion-safe:animate-fadeIn" : "mb-10 ml-4"}>
+      <li ref={ref6} class={inView6 ? "mb-10 ml-4 motion-safe:animate-fadeIn opacity-0" : "mb-10 ml-4 opacity-0"}>
           <div class="absolute w-3 h-3 bg-background rounded-full mt-1.5 -left-1.5 border border-text dark:border-background dark:bg-accent"></div>
           <time class="mb-1 text-sm font-normal leading-none text-primary dark:text-accent">April 2022</time>
           <h3 class="text-lg font-semibold text-background dark:text-text">E-Commerce UI code in Tailwind CSS</h3>
           <p class="text-base font-normal text-accent dark:text-primary">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
       </li>
-      <li ref={ref7} class={inView7 ? "mb-10 ml-4 motion-safe:animate-fadeIn" : "mb-10 ml-4"}>
+      <li ref={ref7} class={inView7 ? "mb-10 ml-4 motion-safe:animate-fadeIn opacity-0" : "mb-10 ml-4 opacity-0"}>
           <div class="absolute w-3 h-3 bg-background rounded-full mt-1.5 -left-1.5 border border-text dark:border-background dark:bg-accent"></div>
           <time class="mb-1 text-sm font-normal leading-none text-primary dark:text-accent">February 2022</time>
           <h3 class="text-lg font-semibold text-background dark:text-text">Application UI code in Tailwind CSS</h3>
@@ -66,13 +66,13 @@ function Portfolio() {
       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
     </svg></a>
       </li>
-      <li ref={ref8} class={inView8 ? "mb-10 ml-4 motion-safe:animate-fadeIn" : "mb-10 ml-4"}>
+      <li ref={ref8} class={inView8 ? "mb-10 ml-4 motion-safe:animate-fadeIn opacity-0" : "mb-10 ml-4 opacity-0"}>
           <div class="absolute w-3 h-3 bg-background rounded-full mt-1.5 -left-1.5 border border-text dark:border-background dark:bg-accent"></div>
           <time class="mb-1 text-sm font-normal leading-none text-primary dark:text-accent">March 2022</time>
           <h3 class="text-lg font-semibold text-background dark:text-text">Marketing UI design in Figma</h3>
           <p class="text-base font-normal text-accent dark:text-primary">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
       </li>
-      <li ref={ref9} class={inView9 ? "mb-10 ml-4 motion-safe:animate-fadeIn" : "mb-10 ml-4"}>
+      <li ref={ref9} class={inView9 ? "mb-10 ml-4 motion-safe:animate-fadeIn opacity-0" : "mb-10 ml-4 opacity-0"}>
           <div class="absolute w-3 h-3 bg-background rounded-full mt-1.5 -left-1.5 border border-text dark:border-background dark:bg-accent"></div>
           <time class="mb-1 text-sm font-normal leading-none text-primary dark:text-accent">April 2022</time>
           <h3 class="text-lg font-semibold text-background dark:text-text">E-Commerce UI code in Tailwind CSS</h3>
