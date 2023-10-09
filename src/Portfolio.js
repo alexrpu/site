548a07c8b02d27ee7f3c4735564be5da1f6f541a
Alex Pu <alexander.r.pu@gmail.com>
2023-10-09 03:06:29 -0700

    updated portfolio

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -83,61 +83,40 @@ function Portfolio() {
       <ol class="relative border-l border-accent col-span-3">
       <li ref={ref} class={inView ? "mb-10 ml-4 motion-safe:animate-fadeIn opacity-0" : "mb-10 ml-4 opacity-0"}>
           <div class="absolute w-3 h-3 bg-accent rounded-full mt-1.5 -left-1.5 border border-background  "></div>
-          <time class="mb-1 text-sm font-normal leading-none text-primary ">February 2022</time>
-          <h3 class="text-lg font-semibold text-text ">Application UI code in Tailwind CSS</h3>
-          <p class="mb-4 text-base font-normal text-primary ">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
-          <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-primary bg-secondary border border-primary rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-accent focus:text-blue-700      ">Learn more <svg class="w-3 h-3 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+          <time class="mb-1 text-sm font-normal leading-none text-primary ">May 2023</time>
+          <h3 class="text-3xl font-semibold text-text ">Innota</h3>
+          <p class="mb-4 text-sm font-normal text-primary ">Practicum</p>
+          <p class="mb-4 text-base font-normal text-text ">An innovative note-taking app that uses AI to assist in explanation and summarization.</p>
+          <a href="https://innotatech.com/" class="inline-flex items-center px-4 py-2 text-sm font-medium text-primary bg-secondary border border-primary rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-accent focus:text-blue-700      ">View Innota<svg class="w-3 h-3 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
     </svg></a>
       </li>
       <li ref={ref2} class={inView2 ? "mb-10 ml-4 motion-safe:animate-fadeIn opacity-0" : "mb-10 ml-4 opacity-0"}>
           <div class="absolute w-3 h-3 bg-accent rounded-full mt-1.5 -left-1.5 border border-background  "></div>
-          <time class="mb-1 text-sm font-normal leading-none text-primary ">February 2022</time>
-          <h3 class="text-lg font-semibold text-text ">Application UI code in Tailwind CSS</h3>
-          <p class="mb-4 text-base font-normal text-primary ">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
+          <time class="mb-1 text-sm font-normal leading-none text-primary ">January 2023 - April 2023</time>
+          <h3 class="text-3xl font-semibold text-text ">BC Centre for Aquatic Health Sciences</h3>
+          <p class="mb-4 text-sm font-normal text-primary ">Practicum</p>
+          <p class="mb-4 text-base font-normal text-text ">A web app that allowed users to filter and view trends in bacteria among sampled water from fisheries.</p>
+          <a href="https://www.cahs-bc.ca/" class="inline-flex items-center px-4 py-2 text-sm font-medium text-primary bg-secondary border border-primary rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-accent focus:text-blue-700      ">View CAHS<svg class="w-3 h-3 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+    </svg></a>
       </li>
       <li ref={ref3} class={inView3 ? "mb-10 ml-4 motion-safe:animate-fadeIn opacity-0" : "mb-10 ml-4 opacity-0"}>
       <div class="absolute w-3 h-3 bg-accent rounded-full mt-1.5 -left-1.5 border border-background  "></div>
-          <time class="mb-1 text-sm font-normal leading-none text-primary ">February 2022</time>
-          <h3 class="text-lg font-semibold text-text ">Application UI code in Tailwind CSS</h3>
-          <p class="mb-4 text-base font-normal text-primary ">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
+          <time class="mb-1 text-sm font-normal leading-none text-primary ">September 2022 - November 2022</time>
+          <h3 class="text-3xl font-semibold text-text ">TravelMe</h3>
+          <p class="mb-4 text-sm font-normal text-primary ">Academic Project</p>
+          <p class="mb-4 text-base font-normal text-text ">An interactive travel companion application for users to see amenities around YVR airport and check flight delays or cancellations.</p>
       </li>
       <li ref={ref4} class={inView4 ? "mb-10 ml-4 motion-safe:animate-fadeIn opacity-0" : "mb-10 ml-4 opacity-0"}>
           <div class="absolute w-3 h-3 bg-accent rounded-full mt-1.5 -left-1.5 border border-background  "></div>
-          <time class="mb-1 text-sm font-normal leading-none text-primary ">February 2022</time>
-          <h3 class="text-lg font-semibold text-text ">Application UI code in Tailwind CSS</h3>
-          <p class="mb-4 text-base font-normal text-primary ">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
-
-      </li>
-      <li ref={ref5} class={inView5 ? "mb-10 ml-4 motion-safe:animate-fadeIn opacity-0" : "mb-10 ml-4 opacity-0"}>
-          <div class="absolute w-3 h-3 bg-accent rounded-full mt-1.5 -left-1.5 border border-background  "></div>
-          <time class="mb-1 text-sm font-normal leading-none text-primary ">February 2022</time>
-          <h3 class="text-lg font-semibold text-text ">Application UI code in Tailwind CSS</h3>
-          <p class="mb-4 text-base font-normal text-primary ">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
-      </li>
-      <li ref={ref6} class={inView6 ? "mb-10 ml-4 motion-safe:animate-fadeIn opacity-0" : "mb-10 ml-4 opacity-0"}>
-          <div class="absolute w-3 h-3 bg-accent rounded-full mt-1.5 -left-1.5 border border-background  "></div>
-          <time class="mb-1 text-sm font-normal leading-none text-primary ">February 2022</time>
-          <h3 class="text-lg font-semibold text-text ">Application UI code in Tailwind CSS</h3>
-          <p class="mb-4 text-base font-normal text-primary ">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
-      </li>
-      <li ref={ref7} class={inView7 ? "mb-10 ml-4 motion-safe:animate-fadeIn opacity-0" : "mb-10 ml-4 opacity-0"}>
-          <div class="absolute w-3 h-3 bg-accent rounded-full mt-1.5 -left-1.5 border border-background  "></div>
-          <time class="mb-1 text-sm font-normal leading-none text-primary ">February 2022</time>
-          <h3 class="text-lg font-semibold text-text ">Application UI code in Tailwind CSS</h3>
-          <p class="mb-4 text-base font-normal text-primary ">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
-      </li>
-      <li ref={ref8} class={inView8 ? "mb-10 ml-4 motion-safe:animate-fadeIn opacity-0" : "mb-10 ml-4 opacity-0"}>
-          <div class="absolute w-3 h-3 bg-accent rounded-full mt-1.5 -left-1.5 border border-background  "></div>
-          <time class="mb-1 text-sm font-normal leading-none text-primary ">February 2022</time>
-          <h3 class="text-lg font-semibold text-text ">Application UI code in Tailwind CSS</h3>
-          <p class="mb-4 text-base font-normal text-primary ">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
-      </li>
-      <li ref={ref9} class={inView9 ? "mb-10 ml-4 motion-safe:animate-fadeIn opacity-0" : "mb-10 ml-4 opacity-0"}>
-          <div class="absolute w-3 h-3 bg-accent rounded-full mt-1.5 -left-1.5 border border-background  "></div>
-          <time class="mb-1 text-sm font-normal leading-none text-primary ">February 2022</time>
-          <h3 class="text-lg font-semibold text-text ">Application UI code in Tailwind CSS</h3>
-          <p class="mb-4 text-base font-normal text-primary ">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
+          <time class="mb-1 text-sm font-normal leading-none text-primary ">April 2022 - May 2022</time>
+          <h3 class="text-3xl font-semibold text-text ">Terra Studium</h3>
+          <p class="mb-4 text-sm font-normal text-primary ">Academic Project</p>
+          <p class="mb-4 text-base font-normal text-text ">A web application that informs users through an interactive web experience about the different causes of climate change.</p>
+          <a href="https://github.com/B-Rabbit7/2800-202210-DTC03" class="inline-flex items-center px-4 py-2 text-sm font-medium text-primary bg-secondary border border-primary rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-accent focus:text-blue-700      ">View Repo<svg class="w-3 h-3 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+    </svg></a>
       </li>
       </ol>
       </div>
